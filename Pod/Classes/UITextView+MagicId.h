9391fcdf4390f02f39c44bb53e70d35f6c674d7b
Alexandre Garrefa <alexmrg@gmail.com>
2015-05-10 14:04:25 -0300

    New category additions to support UITextField, TextView, PickerView, PageControl and Switch

---
+++ Pod/Classes/UITextView+MagicId.h
@@ -0,0 +1,13 @@
+//
+//  UITextView+MagicId.h
+//  Pods
+//
+//  Created by Alexandre Garrefa on 5/10/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (MagicId)
+
+@end
---
+++ Pod/Classes/UITextView+MagicId.m
@@ -0,0 +1,13 @@
+//
+//  UITextView+MagicId.m
+//  Pods
+//
+//  Created by Alexandre Garrefa on 5/10/15.
+//
+//
+
+#import "UITextView+MagicId.h"
+
+@implementation UITextView (MagicId)
+
+@end
